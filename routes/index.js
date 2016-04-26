14f157462c152c8a99ffa49ff390e8943718284b
Luisgarciadefernando <luis.garciadefernandot@alumnos.upm.es>
2016-04-26 14:39:00 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
-  //res.render('index', { footer: 'https://github.com/Luisgarciadefernando/QUIZ-2016.git' });
-
+  //solo un res.render//res.render('index', { title: 'Quiz', url: 'https://github.com/Luisgarciadefernando/QUIZ-2016.git' });
 });
-
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+res.render('author', { url: 'https://github.com/Luisgarciadefernando/QUIZ-2016.git', author: 'Luis García de Fernando' });
+});
 module.exports = router;
