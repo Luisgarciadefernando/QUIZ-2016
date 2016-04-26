5c3ab64e2ede64e858320f427903029f3c19a6e7
Luisgarciadefernando <luis.garciadefernandot@alumnos.upm.es>
2016-04-26 13:55:12 +0200

    Footer

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
+  //res.render('index', { footer: 'https://github.com/Luisgarciadefernando/QUIZ-2016.git' });
+
 });
 
 module.exports = router;
