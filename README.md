30d21161c11087aef64bf443866001411281723e
Luisgarciadefernando <luis.garciadefernandot@alumnos.upm.es>
2016-05-27 18:57:19 +0200

    Base-Datos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # QUIZ-2016
 CORE -2016
+1) Arrancar con: npm start, desde la carpeta de QUIZ
+Para subir commits al GITHUB:
+	1) git add .
+	2) git commit -m "Nombre del commit"
+	3) git push origin master
+
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,30 @@
+var models = require('../models');
+
 // GET /question
-exports.question = function(req, res) {
-	var answer = req.query.answer || '';
-	res.render('quizes/question', {question: 'Capital de Italia', answer: answer});
+exports.question = function(req, res, next) {
+	models.Quiz.findOne().then(function(quiz) {
+		if (quiz) {
+			var answer = req.query.answer || '';
+			res.render('quizes/question', {question: 'Capital de Italia', answer: answer});
+		} else {
+			throw new Error('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error) {
+		next(error);
+	});
 };
 
 // GET /check
-exports.check = function(req, res) {
-	var answer = req.query.answer || '';
-	var result = req.query.answer === 'Roma' ? 'correcta' : 'incorrecta';
-
-	res.render('quizes/result', {result: result, answer: answer});
-}; 
+exports.check = function(req, res, next) {
+	models.Quiz.findOne().then(function(quiz) {
+		if (quiz) {
+			var answer = req.query.answer || '';
+			var result = answer === quiz.answer ? 'correcta' : 'incorrecta';
+			res.render('quizes/result', {result: result, answer: answer});
+		} else {
+			throw new Error('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error) {
+		next(error);
+	});
+};
