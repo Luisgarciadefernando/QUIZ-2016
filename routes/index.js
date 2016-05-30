8f3f81908f97382af9b69ff0164dd0907a0c58aa
Luisgarciadefernando <luis.garciadefernandot@alumnos.upm.es>
2016-05-30 21:47:51 +0200

    Gestion de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var userController = require('../controllers/user_controller');
+
+/* Autoload
++router.param('quizId',quizController.load);
++router.param('userId',userController.load); */
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,11 +18,20 @@ router.get('/author', function(req, res, next) {
 res.render('author', { url: 'https://github.com/Luisgarciadefernando/QUIZ-2016.git', author: 'Luis García de Fernando' });
 });
 
-/* GET question */
 
 /* GET check */
 router.get('/quizes.:format?', quizController.index);
 router.get('/quizes/:quizId(\\d+).:format?', quizController.show);
 router.get('/quizes/:quizId(\\d+)/check', quizController.check);
+
+
+/* GET users */
+router.get('/users', userController.index);
+router.get('/users/:userId(\\d+)', userController.show);
+router.get('/users/new', userController.new);
+router.post('/users', userController.create);
+router.get('/users/:userId(\\d+)/edit', userController.edit);
+router.put('/users/:userId(\\d+)', userController.update);
+router.delete('/users/:userId(\\d+)', userController.destroy);
 
 module.exports = router;
